f9389ea60adab7f3d3d7cc77e3a1e71748044338
Petr Makarov <mr.ru.20042004@gmail.com>
2021-01-13 16:36:12 +0300

    UserImage

---
+++ src/components/UserCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="user-card"></div>
+</template>
+
+<script>
+export default {
+  name: "UserCard",
+};
+</script>
+
+<style>
+.user-card {
+  width: 300px;
+  padding: 20px;
+  border-radius: 10px;
+  margin-top: 100px;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  box-shadow: 0 0 15px 0 rgba(50, 50, 50, 0.2);
+  text-align: center;
+}
+</style>
---
+++ src/components/UserImage.vue
@@ -0,0 +1,19 @@
+<template>
+  <img src="img/70.jpg" class="user-photo" />
+</template>
+
+<script>
+export default {
+  name: "UserImage",
+};
+</script>
+
+<style>
+.user-photo {
+  box-sizing: border-box;
+  height: 150px;
+  margin-top: -100px;
+  border-radius: 50%;
+  border: 3px solid #ccc;
+}
+</style>
